0332bc9b20a4d6d7c2d805b13519bfba7e212909
Roi Abramovitch <roiabr15@gmai.com>
2018-11-22 14:31:53 +0200

    "fixing Junit

---
+++ src/Server_Client/JunitTestChat.java
@@ -0,0 +1,14 @@
+package Server_Client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JunitTestChat {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
